661020f04cc205d3beca29c11ff9939a8b513e1d
Arunscape <8227297+Arunscape@users.noreply.github.com>
2019-10-03 10:36:35 -0600

    formatting

--- assignments/Assignment1/code/commands.c
+++ assignments/Assignment1/code/commands.c
@@ -17,23 +17,17 @@
 void do_commands();
 void attempt_evaluate_from_path();
 
-void determine_what_to_do(struct globals *GLOBALS, char *buffer)
-{
+void determine_what_to_do(struct globals *GLOBALS, char *buffer) {
 
   // begin semicolon handling
   char *buffer_copy = strdup(buffer);
   char *semicolon_token = strtok(buffer_copy, ";");
-  char *buffer_copy2 = strdup(semicolon_token);
-  while ((semicolon_token = strtok(NULL, ";")) != NULL)
-  {
-    printf("Here's what got separated: %s\n", (char *)semicolon_token);
-    determine_what_to_do(GLOBALS, semicolon_token);
-  }
+  char *first_semicolon_token = strdup(semicolon_token);
 
   // end semicolon handling
 
   struct array tokens = create_array(sizeof(char *));
-  tokenize(buffer_copy2, " ", &tokens); // &mut tokens
+  tokenize(first_semicolon_token, " ", &tokens); // &mut tokens
 
   // // begin redirection handling
   // int index_of_redir = -1;
@@ -49,82 +43,64 @@ void determine_what_to_do(struct globals *GLOBALS, char *buffer)
 
   // begin pipe handling
   // end pipe handling
-  if (false)
-  {
+  if (false) {
     ;
-  }
-  else
-  { // normal stdin and stdout
+  } else { // normal stdin and stdout
     do_commands(&tokens, GLOBALS, stdin, stdout, stderr);
   }
-  free(tokens.array_ptr);
-  free(buffer_copy2);
+
+  while ((semicolon_token = strtok(NULL, ";")) != NULL) {
+    printf("Here's what got separated: %s\n", (char *)semicolon_token);
+    determine_what_to_do(GLOBALS, semicolon_token);
+  }
+  free(buffer_copy);
+  free(first_semicolon_token);
   free(semicolon_token);
+  free(tokens.array_ptr);
 }
 
-void do_commands(struct array *tokens, struct globals *GLOBALS, FILE *sin, FILE *sout, FILE *serr)
-{
+void do_commands(struct array *tokens, struct globals *GLOBALS, FILE *sin,
+                 FILE *sout, FILE *serr) {
 
-  if (strcmp(*((char **)get_from_array(tokens, 0)), "cd") == 0)
-  {
-    if (tokens->size > 2)
-    {
+  if (strcmp(*((char **)get_from_array(tokens, 0)), "cd") == 0) {
+    if (tokens->size > 2) {
       fprintf(stderr, "Too many args for cd command, expected 1\n");
       return;
-    }
-    else if (tokens->size < 1)
-    {
+    } else if (tokens->size < 1) {
       fprintf(stderr, "Expected one arg for cd\n");
       return;
     }
 
-    if (chdir(*((char **)get_from_array(tokens, 1))) == -1)
-    {
+    if (chdir(*((char **)get_from_array(tokens, 1))) == -1) {
       perror("chdir");
     }
-  }
-  else if (strcmp(*((char **)get_from_array(tokens, 0)), "pwd") == 0)
-  {
-    if (tokens->size > 1)
-    {
+  } else if (strcmp(*((char **)get_from_array(tokens, 0)), "pwd") == 0) {
+    if (tokens->size > 1) {
       fprintf(stderr, "Expected 0 args for pwd\n");
       return;
     }
     char cwd[PATH_MAX];
-    if (getcwd(cwd, PATH_MAX) != NULL)
-    {
+    if (getcwd(cwd, PATH_MAX) != NULL) {
       printf("%s\n", cwd);
-    }
-    else
-    {
+    } else {
       perror("getcwd() error");
     }
-  }
-  else if (strcmp(*((char **)get_from_array(tokens, 0)), "$PATH") == 0)
-  {
-    if (tokens->size > 1)
-    {
+  } else if (strcmp(*((char **)get_from_array(tokens, 0)), "$PATH") == 0) {
+    if (tokens->size > 1) {
       fprintf(stderr, "Expected 0 args for $PATH\n");
       return;
     }
     fprintf(stdout, "Current PATH: %s\n", GLOBALS->PATH);
-  }
-  else if (strcmp(*((char **)get_from_array(tokens, 0)), "a2path") == 0)
-  {
-    if (tokens->size > 2)
-    {
+  } else if (strcmp(*((char **)get_from_array(tokens, 0)), "a2path") == 0) {
+    if (tokens->size > 2) {
       fprintf(stderr, "Too many arguments, expected 1 arg for a2path\n");
       return;
-    }
-    else if (tokens->size < 2)
-    {
+    } else if (tokens->size < 2) {
       fprintf(stderr, "Expected 1 arg for a2path\n");
       return;
-    }
-    else if (strlen(*((char **)get_from_array(tokens, 1))) < 7 ||
-             strncmp(*((char **)get_from_array(tokens, 1)), "$PATH:", 6) !=
-                 0)
-    {
+    } else if (strlen(*((char **)get_from_array(tokens, 1))) < 7 ||
+               strncmp(*((char **)get_from_array(tokens, 1)), "$PATH:", 6) !=
+                   0) {
       fprintf(stderr, "Incorrect syntax for 2nd arg in a2path. It should start "
                       "with $PATH: and then have a path to add.\nFor example: "
                       "a2path $PATH:/usr/local/bin\n");
@@ -140,24 +116,18 @@ void do_commands(struct array *tokens, struct globals *GLOBALS, FILE *sin, FILE 
            strchr(*((char **)get_from_array(tokens, 1)), (int)":") - 1);
     free(GLOBALS->PATH);
     GLOBALS->PATH = new_path;
-  }
-  else if ((strcmp(*((char **)get_from_array(tokens, 0)), "exit")) == 0)
-  {
-    if (tokens->size > 1)
-    {
+  } else if ((strcmp(*((char **)get_from_array(tokens, 0)), "exit")) == 0) {
+    if (tokens->size > 1) {
       fprintf(stderr, "Expected 0 args for exit\n");
       return;
     }
     GLOBALS->EXIT = true;
-  }
-  else
-  {
+  } else {
     attempt_evaluate_from_path(tokens, GLOBALS);
   }
 }
 
-void attempt_evaluate_from_path(struct array *tokens, struct globals *GLOBALS)
-{
+void attempt_evaluate_from_path(struct array *tokens, struct globals *GLOBALS) {
 
   struct array paths = create_array(sizeof(char *));
   char *path_copy = strdup(GLOBALS->PATH);
@@ -165,8 +135,7 @@ void attempt_evaluate_from_path(struct array *tokens, struct globals *GLOBALS)
   tokenize(path_copy, ":", &paths); // &mut paths
 
   // next time, loop over strtok
-  for (int i = 0; i < paths.size; i += 1)
-  {
+  for (int i = 0; i < paths.size; i += 1) {
     char *concat = NULL;
     asprintf(&concat, "%s/%s", *(char **)get_from_array(&paths, i),
              *(char **)get_from_array(tokens, 0));
@@ -180,31 +149,27 @@ void attempt_evaluate_from_path(struct array *tokens, struct globals *GLOBALS)
   push_to_array(&paths, &concat);
 
   __pid_t f = fork();
-  if (!f) //child
+  if (!f) // child
   {
-    if (access(*(char **)get_from_array(tokens, 0), F_OK | X_OK) == 0)
-    { //attempt to execute the absolute path
+    if (access(*(char **)get_from_array(tokens, 0), F_OK | X_OK) ==
+        0) { // attempt to execute the absolute path
       execve(*(char **)get_from_array(tokens, 0), tokens->array_ptr, NULL);
     }
-    for (int i = 0; i < paths.size; i += 1)
-    {
+    for (int i = 0; i < paths.size; i += 1) {
 
-      if (access(*(char **)get_from_array(&paths, i), F_OK | X_OK) == 0)
-      {
+      if (access(*(char **)get_from_array(&paths, i), F_OK | X_OK) == 0) {
         execve(*(char **)get_from_array(&paths, i), tokens->array_ptr, NULL);
       }
 
-      else
-      {
+      else {
         ;
       }
     }
   }
-  wait(NULL); //wait for child process to terminate
+  wait(NULL); // wait for child process to terminate
 
   // cleanup
-  for (int i = 0; i < paths.size; i += 1)
-  {
+  for (int i = 0; i < paths.size; i += 1) {
     free(*(void **)get_from_array(&paths, i));
   }
   free(paths.array_ptr);
--- assignments/Assignment1/code/dragonshell.c
+++ assignments/Assignment1/code/dragonshell.c
@@ -11,8 +11,7 @@
 #include "util.h"
 #include "welcome.h"
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
   // struct globals GLOBALS = {
   //     .PATH = (char *)"/bin/:/usr/bin",
   //     .EXIT = 0,
@@ -25,19 +24,17 @@ int main(int argc, char **argv)
   strcpy(GLOBALS.PATH, "/bin/:/usr/bin");
   printDragon();
 
-  for (;;)
-  {
+  for (;;) {
 
     printf("\n🐲 dragonshell🐉 > ");
 
     char *buffer = NULL;
     size_t buffer_size = 0;
-    getline(&buffer, &buffer_size, stdin);  // &mut buffer &mut buffer_size
-    strtok(buffer, "\n");                   // get rid of \n at the end of the line
+    getline(&buffer, &buffer_size, stdin); // &mut buffer &mut buffer_size
+    strtok(buffer, "\n"); // get rid of \n at the end of the line
     determine_what_to_do(&GLOBALS, buffer); // &mut GLOBALS
     free(buffer);
-    if (GLOBALS.EXIT)
-    {
+    if (GLOBALS.EXIT) {
       free(GLOBALS.PATH);
       break;
       // _exit(0);
