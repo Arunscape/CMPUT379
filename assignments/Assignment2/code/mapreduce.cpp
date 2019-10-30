4c1ce6e7d90e86371848129d1e9c654fd405fcef
Arunscape <8227297+Arunscape@users.noreply.github.com>
2019-10-29 18:14:35 -0600

    add shit to the queue

--- assignments/Assignment2/code/mapreduce.cpp
+++ assignments/Assignment2/code/mapreduce.cpp
@@ -1,9 +1,20 @@
 #include "mapreduce.h"
+#include "threadpool.h"
+#include "reeeee.h"
 
 void MR_Run(int num_files, char *filenames[],
             Mapper map, int num_mappers,
             Reducer concate, int num_reducers){
-  ;
+  
+  ThreadPool_t* threadpool = ThreadPool_create(num_mappers);
+
+  for (int i=0; i < num_files; i+=1){
+    bool addedwork = ThreadPool_add_work(threadpool,(thread_func_t) map, filenames[i]);
+    if (!addedwork){
+      REEEEE("Error adding work to threadpool");
+    }
+      
+  }
 }
 
 void MR_Emit(char *key, char *value){
