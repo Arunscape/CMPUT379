99229f387feeb5a0b924ad8df6fdf348627c453a
Arunscape <8227297+Arunscape@users.noreply.github.com>
2019-10-29 23:41:07 -0600

    maybe it will look like this

--- assignments/Assignment2/code/mapreduce.cpp
+++ assignments/Assignment2/code/mapreduce.cpp
@@ -1,14 +1,18 @@
 #include <vector>
 #include <utility>
+#include <algorithm>
+
 #include <pthread.h>
 
 #include "mapreduce.h"
 #include "threadpool.h"
 #include "reeeee.h"
 
+struct partition;
 
 //datastruct| partition|           key    value
-std::vector<std::vector<std::pair<char*, char*>>> shared_data;
+// std::vector<std::vector<std::pair<char*, char*>>> shared_data;
+std::vector<partition> shared_data;
 
 struct partition {
   pthread_mutex_t mutex;
@@ -21,6 +25,7 @@ struct partition {
   }
 
 };
+
 
 void MR_Run(int num_files, char *filenames[],
             Mapper map, int num_mappers,
@@ -37,12 +42,31 @@ void MR_Run(int num_files, char *filenames[],
   }
 }
 
-void MR_Emit(char *key, char *value){
-  ;
+// this function was copied from the assignment description
+unsigned long MR_Partition(char* key, int num_partitions){
+  unsigned long hash = 5381;
+  int c;
+  while ((c=*key++) != '\0')
+    hash = hash * 33 + c;
+  return hash % num_partitions;
 }
 
-unsigned long MR_Partition(char *key, int num_partitions){
-  return 0;
+
+bool compareByLength(const data &a, const data &b)
+{
+    return a.word.size() < b.word.size();
+}
+
+void MR_Emit(char *key, char *value){
+  unsigned long partno = MR_Partition(key, num_partitions);
+  std::pair<char*, char*> pair = std::make_pair(key, value);
+
+  pthread_mutex_lock(&shared_data[partno]->mutex)
+  shared_data[partno].insert(
+        std::upper_bound(shared_date[partno].cbegin(), shared_data[partno].cend(), pair),
+        pair
+      );
+  pthread_mutex_unlock(&shared_data[partno]->mutex)
 }
 
 void MR_ProcessPartition(int partition_number){
