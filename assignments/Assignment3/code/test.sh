2e84e7003ff89ed890ed98ec1e7d1148857aaaeb
Arunscape <8227297+Arunscape@users.noreply.github.com>
2019-12-02 18:43:59 -0700

    modify test.sh

--- assignments/Assignment3/code/test.sh
+++ assignments/Assignment3/code/test.sh
@@ -5,9 +5,9 @@ echo M disk0 > test.txt
 echo C first 64 >> test.txt
 ./fs test.txt
 
-#echo M disk0 > test.txt
-#echo C sec 64 >> test.txt
-#./fs test.txt
-#
-#echo M disk0 > test.txt
-#./fs test.txt
+echo M disk0 > test.txt
+echo C sec 63 >> test.txt
+./fs test.txt
+
+echo M disk0 > test.txt
+./fs test.txt
